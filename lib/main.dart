980f40823b47ce44c55a4cc432186b40491a6c56
Vitafresh <vvitafresh@gmail.com>
2022-10-02 15:27:03 +0300

    029 Visible (Input _ Output) & Invisible (Layout _ Control) Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:legacy_buttons/LegacyRaisedButton.dart';
 
 void main() {
   runApp(MyApp());
@@ -6,12 +7,32 @@ void main() {
 
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favourite color?',
+      'What\'s your favourite animal?'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('My title'),
         ),
-        body: const Text('This is my text'),
+        body: Column(
+          children: <Widget>[
+            Text('This is a text!'),
+            LegacyRaisedButton(
+              child: Text('Answer 1'),
+              onPressed: null,
+            ),
+            LegacyRaisedButton(
+              child: Text('Answer 2'),
+              onPressed: null,
+            ),
+            LegacyRaisedButton(
+              child: Text('Answer 3'),
+              onPressed: null,
+            ),
+          ],
+        ),
       ),
     );
   }
